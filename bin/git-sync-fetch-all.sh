809ac78d16a1095e6a3421acc468a303e7917b0c
Goshine <greenlig@gmail.com>
2016-05-12 15:54:21 +0100

    fix(update git config): update the git script
    I have update the git script in order do a git fetch before other operations
    
    * bin/git-sync-fetch-all.sh

--- bin/git-sync-fetch-all.sh
+++ bin/git-sync-fetch-all.sh
@@ -1,14 +1,13 @@
 #!/bin/bash 
 cd $1;
 
-rm list.text;
 for i in `ls -lrt -d -1 $PWD/*`; 
   do
     echo $i  ; 
     cd $i;
+    git fetch --all
     git pull && composer install;
     echo $i \n
     echo "=================";
     cd ../;
 done 
-# git remote add upstream {upstream-repo.git}
