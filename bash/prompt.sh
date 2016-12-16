2280cf1d514e463eee723b5a32098d28d5aa1ef9
Aaron Goshine <greenlig@gmail.com>
2016-12-16 11:36:57 +0000

    fix(dynamic user name): removed -- at the end

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -4,7 +4,7 @@ echo '     /  | ALL WAYS RTFMP.'
 echo '    / - |_  _____ ___  ___ ____ ________'
 echo '   / __ | |/ / -_\ _ \/ _  / -_) __/ __/'
 echo '  / / |_|___/\__/_//_/\__/ \__/_/  /__/ '
-echo " /_/  THE AGE OF ULTRA-$(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') --"
+echo " /_/  THE AGE OF ULTRA-$(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') "
 
 
  PS1='> \h [\W\[\e[1;32m\]$(__git_ps1 "(%s)")\[\e[0m\]]\[\e[1;32m\] •--➤ \[\e[0m\]'
