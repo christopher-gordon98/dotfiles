9b244c42aef00149a096c0bc086aeb80d1cfc244
Aaron Goshine <greenlig@gmail.com>
2016-12-23 12:11:25 +0000

    feat(Local env template): Added a local env tmplt
    - Create a simple starter file to hold local environment
    - setting and scripts and configurations

---
+++ bash/localenv-template.sh
@@ -0,0 +1,9 @@
+
+export _DATABASE_URL=""
+export HEROKU_POSTGRESQL_PINK_URL=""
+alias work="cd ~/work/"
+export SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'`
+export GIT_AUTHOR_NAME="" 
+export GIT_COMMITTER_NAME="" 
+export GIT_AUTHOR_EMAIL=""
+export GIT_COMMITTER_EMAIL="" 
