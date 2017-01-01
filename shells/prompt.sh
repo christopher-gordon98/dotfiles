22cd69ebae2203e7d6fe30f20934e6afdf155af7
Aaron Goshine <greenlig@gmail.com>
2017-01-01 22:29:51 +0000

    feat(Prompt): Implement new Ultra design

--- shells/prompt.sh
+++ shells/prompt.sh
@@ -1,7 +1,26 @@
-echo '      __ '
-echo '     /  | ALL WAYS RTFMP.'   
-echo '    / - |_  _____ ___  ___ ____ ________'
-echo '   / __ | |/ / -_\ _ \/ _  / -_) __/ __/'
-echo '  / / |_|___/\__/_//_/\__/ \__/_/  /__/ '
-echo " /_/  THE AGE OF ULTRA-$(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') "
+getUltraStr() {
+  MINWIDTH=60
+  ULNAMEMAX=$(expr $(tput cols) - 43)
 
+  if [  "$(tput cols)" -lt "$MINWIDTH" ]
+  then
+    echo "$(printf "%*s\n" $ULNAMEMAX | tr " " "-")" && return;
+  fi
+
+  ULTRANAMELEN=$1
+  PADD="$(expr $(expr $ULNAMEMAX - ${#ULTRANAMELEN}) / 2)"
+  PADDCHARS="$(printf "%*s\n" $PADD | tr " " "-")"
+    echo "$PADDCHARS$1$PADDCHARS"
+  }
+  TITLE=`getUltraStr ' ALL WAYS RTFMP '` 
+  ULTRANAME=`getUltraStr " ULTRA $(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') "` 
+  FMN=`getUltraStr ' ✿ '` 
+  RULE=`getUltraStr ''` 
+
+  echo " "
+  echo "██╗   ██╗██╗  ████████╗██████╗  █████╗  $TITLE"
+  echo "██║   ██║██║  ╚══██╔══╝██╔══██╗██╔══██╗ $RULE"
+  echo "██║   ██║██║     ██║   ██████╔╝███████║ $FMN"
+  echo "██║   ██║██║     ██║   ██╔══██╗██╔══██║ $RULE"
+  echo "╚██████╔╝███████╗██║   ██║  ██║██║  ██║ $ULTRANAME"
+  echo " ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ $RULE"
