7ef60dac2589814c907beefa9019628aee3fad1e
Aaron Goshine <greenlig@gmail.com>
2017-01-02 10:49:48 +0000

    chore(Unused): Remove unused scripts

--- bin/usefullcmds.sh
+++
@@ -1,5 +0,0 @@
-sed -i 's/pattern/replacement/' <files>
-ind . -name *.cpp | xargs sed -i 's/pattern/replacement/'
-grep -rl 'pattern1' | xargs sed -i 's/pattern2/replacement/'
-ack -l 'pattern' | xargs perl -pi -E 's/pattern/replacement/g'
-
