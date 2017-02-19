8e30a9809135aca568298f9711d7c3016f0a08b1
Aaron Goshine <greenlig@gmail.com>
2017-02-19 10:17:30 +0000

    feat(Shell): Simplify prompt

--- shells/prompt.sh
+++ shells/prompt.sh
@@ -1,26 +1,28 @@
 source $HOME/.dotfiles/shells/functions.sh
 
-TITLE=$(get_ultra_rule_str ' ALWAYS RTFMP ')
-ULTRANAME=$(get_ultra_rule_str " ULTRA $(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') ")
-RULE=$(get_ultra_rule_str '') 
-FMNO=$(get_ultra_rule_str ' ✿ ') 
+TITLE=$(get_ultra_rule_str ' ALWAYS RTFMP ' 0 0)
+ULTRANAME=$(get_ultra_rule_str " ✿ ULTRA $(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') ✿ " 0 0)
+RULE=$(get_ultra_rule_str '' 0 0) 
+FMNO=$(get_ultra_rule_str ' ✿ ' 0 0) 
 
-echo " "
-echo "██╗   ██╗██╗  ████████╗██████╗  █████╗  $TITLE"
-echo "██║   ██║██║  ╚══██╔══╝██╔══██╗██╔══██╗ $RULE"
-echo "██║   ██║██║     ██║   ██████╔╝███████║ $FMNO"
-echo "██║   ██║██║     ██║   ██╔══██╗██╔══██║ $RULE"
-echo "╚██████╔╝███████╗██║   ██║  ██║██║  ██║ $ULTRANAME"
-echo " ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ $RULE"
+function ultra_prompt () {
+  TITLE=$(get_ultra_rule_str ' ALWAYS RTFMP ')
+  ULTRANAME=$(get_ultra_rule_str " ULTRA $(whoami | sed 's/\.[a-z]*$//' | awk '{print toupper($0)}') ")
+  RULE=$(get_ultra_rule_str '') 
+  FMNO=$(get_ultra_rule_str ' ✿ ') 
 
-THEME_PROMPT_DIRTY="[✷]"
-THEME_PROMPT_CLEAN="(⦿)"
+  echo " "
+  echo "██╗   ██╗██╗  ████████╗██████╗  █████╗  $TITLE"
+  echo "██║   ██║██║  ╚══██╔══╝██╔══██╗██╔══██╗ $RULE"
+  echo "██║   ██║██║     ██║   ██████╔╝███████║ $FMNO"
+  echo "██║   ██║██║     ██║   ██╔══██╗██╔══██║ $RULE"
+  echo "╚██████╔╝███████╗██║   ██║  ██║██║  ██║ $ULTRANAME"
+  echo " ╚═════╝ ╚══════╝╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ $RULE"
+}
 
-THEME_PROMPT_PREFIX="-|"
-SCM_THEME_PROMPT_SUFFIX="|"
-
-GIT_THEME_PROMPT_PREFIX="-|"
-GIT_THEME_PROMPT_SUFFIX="|"
+echo "$ULTRANAME"
+echo "$RULE"
+t1=$(get_ultra_rule_str ' Ultra dotfiles installer ' 0 0)
 
 source "$(brew --prefix git)/etc/bash_completion.d/git-prompt.sh"
-PS1='> \h [\W\[\e[1;32m\]$(__git_ps1 "(%s)")\[\e[0m\]]\[\e[1;32m\] ➜ \[\e[0m\]'
+PS1='⦿ \h [\W\[\e[1;32m\]$(__git_ps1 "(%s)")\[\e[0m\]]\[\e[1;32m\] ▶︎ \[\e[0m\]'
