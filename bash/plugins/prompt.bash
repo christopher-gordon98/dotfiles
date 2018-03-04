963ef0cb36d1488d4dd94df39838e8bb25960a9b
Aaron Goshine <greenlig@gmail.com>
2018-03-04 11:39:20 +0000

    feat(Bash): implement a prompt for slower machines

--- bash/plugins/prompt.bash
+++ bash/plugins/prompt.bash
@@ -22,8 +22,13 @@ echo "$ULTRANAME"
 echo "$RULE"
 t1=$(get_ultra_rule_str ' Ultra dotfiles installer ' 0 0)
 
+if [ $FAST_MACHINE ]
+then
+export GIT_PS1_SHOWCOLORHINTS=1
+export GIT_PS1_SHOWSTASHSTATE=1
+export GIT_PS1_SHOWDIRTYSTATE=1
 source "$(brew --prefix git)/etc/bash_completion.d/git-prompt.sh"
-GIT_PS1_SHOWCOLORHINTS=1
-GIT_PS1_SHOWSTASHSTATE=1
-GIT_PS1_SHOWDIRTYSTATE=1
 PROMPT_COMMAND='__git_ps1 "⦿ (\[\e[1;32m\]`basename \"$VIRTUAL_ENV\"`\[\e[0m\])[\W]" "\[\e[1;32m\]--❯ \[\e[0m\]"'
+else
+  export PS1="⦿ \H[\W](\e[0;32m\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)\e[m) \e[0;32m--❯ \e[m "
+fi
